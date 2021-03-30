276e6d6cacedf7f29a0f6576c241cf994d195294
mendax1234 <mendax1234@sina.cn>
2021-03-30 17:28:53 +0800

    Return something to normal

--- 2021FRC/src/main/java/frc/robot/commands/setFastCmd.java
+++ 2021FRC/src/main/java/frc/robot/commands/setFastCmd.java
@@ -22,7 +22,7 @@ public class setFastCmd extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    Robot.m_Drive.setFast();
+    //Robot.m_Drive.setFast();
     //todo
   }
 
